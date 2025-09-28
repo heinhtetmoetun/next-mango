b9240a3a85d5c5ef69ca49cbad97d252a8bfab5b
heinhtetmoetun <u6530394@au.edu>
2025-09-28 23:10:55 +0700

    Add order field to Category model

--- models/Category.js
+++ models/Category.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const CategorySchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
+  order: { type: Number, default: 0 }, // ✅ required by teacher for sorting
 });
 
 export default mongoose.models.Category || mongoose.model("Category", CategorySchema);
